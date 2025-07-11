abe4d52b452c584ac1de2f2a42fab7cfa3a721d7
Neelima <neelutgd507@gmail.com>
2025-07-11 15:14:56 -0700

    byee

--- eclipse-workspace/CucumberFramework/src/test/java/pageObjects/GitPractice.java
+++ eclipse-workspace/CucumberFramework/src/test/java/pageObjects/GitPractice.java
@@ -3,7 +3,7 @@ package pageObjects;
 public class GitPractice {
 	public void gitPrac() {
 	System.out.println("Hello from Git Practice Class! *******");
-	System.out.println("Hello *******");}  
+	System.out.println("Hello waste");}  
 
 
 }
