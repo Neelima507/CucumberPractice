56f6ad6a57956e0616c46ab75bf0710b43ad75aa
Neelima507 <Neelutgd507@gmail.com>
2025-07-11 15:03:18 -0700

    Update GitPractice.java

--- eclipse-workspace/CucumberFramework/src/test/java/pageObjects/GitPractice.java
+++ eclipse-workspace/CucumberFramework/src/test/java/pageObjects/GitPractice.java
@@ -2,7 +2,7 @@ package pageObjects;
 
 public class GitPractice {
 	public void gitPrac() {
-	System.out.println("Hello from Git Practice Class!");}  
+	System.out.println("Hello from Git Practice Class! *******");}  
 
 
 }
